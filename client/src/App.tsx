c27e9c760efd0ca501867f2d2ad31b99938ac21a
Gizem <109289446+gizemayegul@users.noreply.github.com>
2024-05-04 08:56:18 +0200

    comments are deleted

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,18 +11,6 @@ const API_URL = import.meta.env.VITE_SERVER_URL;
 console.log(API_URL);
 
 function App() {
-  // const [data, setData] = useState<string>("");
-
-  // useEffect(() => {
-  //   (async function () {
-  //     try {
-  //       const response = await axios.get(API_URL);
-  //       setData(response.data);
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   })();
-  // }, []);
   return (
     <>
       {/* <div className="text-3xl font-bold underline">{data}</div> */}
