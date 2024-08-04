5211ee4cec0ab0e0ca4b0cc319eada3a3340873c
Gizem <109289446+gizemayegul@users.noreply.github.com>
2024-08-04 20:39:59 +0200

    Vercel

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -6,10 +6,9 @@ import mongoose from "mongoose";
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  `mongodb://127.0.0.1:27017/${process.env.MONGODB_URI}` ||
-  "mongodb://127.0.0.1:27017/fullstack";
+  `${process.env.MONGODB_URI}` ||
+  `mongodb://127.0.0.1:27017/${process.env.DB_NAME}`;
 console.log(MONGO_URI);
-console.log(process.env.MONGODB_URI, "fb");
 
 mongoose
   .connect(MONGO_URI)
