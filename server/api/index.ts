c6c8c5e64adeca1be9c2406e132ba8dc7294967e
Gizem <109289446+gizemayegul@users.noreply.github.com>
2024-08-05 14:11:05 +0200

    vercel config change

--- server/api/index.ts
+++ server/api/index.ts
@@ -30,8 +30,26 @@ console.log(`PORT: ${process.env.PORT}`);
 console.log(`HOST: ${process.env.HOST}`);
 console.log(`MONGODB_URI: ${process.env.MONGODB_URI}`);
 
-import "./src/db";
-import app from "./app";
+import "../src/db";
+// import app from "../app";
+import express, { Application } from "express";
+import userRoute from "../src/routes/user.routes";
+import errorHandler from "../src/error-handling/index";
+import projectRoute from "../src/routes/project.routes";
+import boardRoute from "../src/routes/board.routes";
+import columnRoute from "../src/routes/column.routes";
+import middleWare from "../src/config/index";
+
+const app: Application = express();
+middleWare(app);
+
+app.use("/", userRoute);
+app.use("/", projectRoute);
+app.use("/", boardRoute);
+app.use("/", columnRoute);
+
+errorHandler(app);
+export default app;
 
 app.listen(Number(port), host, () => {
   console.log(`Server listening on ${host}:${port}`);
