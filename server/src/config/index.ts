819a1910f6d2be08adf12fc3b71d11968e408479
Gizem <109289446+gizemayegul@users.noreply.github.com>
2024-08-05 13:06:09 +0200

    remove vercel

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -22,6 +22,7 @@ const middleWare = (app: Application) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
+  app.use(express.static("public"));
 };
 
 export default middleWare;
