35d859f1cb701d644fc84550c943fd4f940ac250
Gizem <109289446+gizemayegul@users.noreply.github.com>
2024-08-14 10:25:42 +0200

    landing-page

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -13,7 +13,7 @@ export default function Home() {
             // style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
           ></div>
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="mx-auto max-w-2xl py-32 max-sm:py-8 lg:py-56 ">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className=" relative rounded-full px-3 py-1 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
               Announcing our next round of funding.{" "}
@@ -27,10 +27,10 @@ export default function Home() {
             </div>
           </div>
           <div className="text-center">
-            <h1 className=" dark:text-white text-4xl font-bold tracking-tight  sm:text-6xl">
+            <h1 className=" dark:text-white text-4xl font-bold tracking-tight  sm:text-6xl max-sm:text-4xl">
               Boost your productivity by using our application
             </h1>
-            <p className=" mt-6 text-lg leading-8text-center">
+            <p className=" mt-6 text-lg leading-8text-center max-sm:text-md">
               Discover the ultimate solution to enhance your productivity. Our
               application is designed to streamline your tasks, allowing you to
               focus on what truly matters. Experience unparalleled efficiency
