832f08853501ed3a51cdefa4e7f57ab72477e371
Gizem <109289446+gizemayegul@users.noreply.github.com>
2024-11-10 09:38:02 +0100

    run unit test and have ci/cd

---
+++ client/src/Pages/Home/Home.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Home from "../../Pages/Home/Home";
+import { BrowserRouter } from "react-router-dom";
+
+describe("Home", () => {
+  test("renders home page", () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    expect(
+      screen.getByText("Boost your productivity by using our application")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("linki want to ha", { name: /Get started/i })
+    ).toBeInTheDocument();
+  });
+});
